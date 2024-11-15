4b440aced80355b5aa34f1e8860c45658eb5f84e
Michel Zehnder <MichelZ@users.noreply.github.com>
2024-11-15 14:06:35 +0100

    Move FillDoubleBytes/FillFloatBytes to Shared TdsParser for future use

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/TdsParser.NetCoreApp.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/TdsParser.NetCoreApp.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Diagnostics;
-using System.Buffers.Binary;
 
 namespace Microsoft.Data.SqlClient
 {
@@ -12,10 +11,6 @@ namespace Microsoft.Data.SqlClient
     {
         internal static void FillGuidBytes(Guid guid, Span<byte> buffer) => guid.TryWriteBytes(buffer);
 
-        internal static void FillDoubleBytes(double value, Span<byte> buffer) => BinaryPrimitives.TryWriteInt64LittleEndian(buffer, BitConverter.DoubleToInt64Bits(value));
-
-        internal static void FillFloatBytes(float value, Span<byte> buffer) => BinaryPrimitives.TryWriteInt32LittleEndian(buffer, BitConverterCompatible.SingleToInt32Bits(value));
-        
         internal static Guid ConstructGuid(ReadOnlySpan<byte> bytes)
         {
             Debug.Assert(bytes.Length >= 16, "not enough bytes to set guid");
